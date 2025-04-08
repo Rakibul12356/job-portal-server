26dc33d9451992d86f617a468f05f36de61eb129
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-04-08 06:22:15 +0600

    single job details get operation done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app= express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 //VZ29RsT4Qr6XSJnC
-//r
+//
 app.use(cors())
 app.use(express.json())
 
@@ -35,6 +35,12 @@ async function run() {
     app.get('/jobs',async(req,res)=>{
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
+      res.send(result)
+    })
+    app.get('/jobs/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await jobsCollection.findOne(query);
       res.send(result)
     })
 
